a789f8f96e3ef38860001d28992896c873b2538f
Kruksii <wowkruksi@gmail.com>
2023-11-07 12:25:02 +0100

    Add ExportFolderRights

---
+++ src/export/ExportFolderRights.ps1
@@ -0,0 +1,17 @@
+﻿# CONFIGURATION
+$OutFile = "C:\Temp\Permissions1.csv"
+$RootPath = "\\LOCALHOST\SHARE"
+
+# EXEC
+$Header = "Folder Path,IdentityReference,AccessControlType,FileSystemRights,IsInherited,InheritanceFlags,PropagationFlags"
+$FileExist = Test-Path $OutFile 
+If ($FileExist -eq $True) { Remove-Item $OutFile } 
+Add-Content -Value $Header -Path $OutFile 
+$Folders = Get-ChildItem $RootPath -Recurse | Where-Object { $_.PSIsContainer -eq $true } 
+foreach ($Folder in $Folders) {
+    $ACLs = Get-Acl $Folder.FullName | ForEach-Object { $_.Access }
+    foreach ($ACL in $ACLs) {
+        $OutInfo = $Folder.FullName + "," + $ACL.IdentityReference.ToString() + "," + $ACL.AccessControlType + "," + $ACL.FileSystemRights + "," + $ACL.IsInherited + "," + $ACL.InheritanceFlags + "," + $ACL.PropagationFlags
+        Add-Content -Value $OutInfo -Path $OutFile 
+    }
+}
